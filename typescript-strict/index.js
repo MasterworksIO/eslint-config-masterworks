a4226bc79bf20f16196b56a5d1c3f514f222d8bf
Stefan Maric <me@stefanmaric.com>
2021-11-29 08:08:57 -0500

    Move some rules out of typescript into strict and stylish variants [f:1h]
    Also remove react/prop-types to prevent missing peer dependencies
    issues on projects that don't use react.

--- typescript-strict/index.js
+++ typescript-strict/index.js
@@ -1,17 +1,19 @@
 module.exports = {
   rules: {
+    '@typescript-eslint/explicit-function-return-type': ['error'],
     '@typescript-eslint/no-implicit-any-catch': ['error'],
     '@typescript-eslint/no-inferrable-types': ['warn'],
+    '@typescript-eslint/no-non-null-assertion': ['error'],
     '@typescript-eslint/no-redeclare': ['error'],
     '@typescript-eslint/no-shadow': ['error'],
     '@typescript-eslint/no-this-alias': ['error'],
-    '@typescript-eslint/no-non-null-assertion': ['error'],
-    '@typescript-eslint/explicit-function-return-type': ['error'],
     '@typescript-eslint/no-unsafe-argument': ['error'],
     '@typescript-eslint/no-unsafe-assignment': ['error'],
     '@typescript-eslint/no-unsafe-call': ['error'],
     '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/switch-exhaustiveness-check': ['error'],
+    '@typescript-eslint/prefer-enum-initializers': ['error'],
+    '@typescript-eslint/prefer-literal-enum-member': ['error'],
     '@typescript-eslint/promise-function-async': ['error'],
+    '@typescript-eslint/switch-exhaustiveness-check': ['error'],
   },
 }
